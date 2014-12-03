8f76e411705337ac3110c2f17491286ddffe753c
Gregory Mitrano <blairmlh@gmail.com>
2014-12-03 08:04:48 -0500

    Finished most of the sync track work

--- src/core/SyncTrack.c
+++ src/core/SyncTrack.c
@@ -7,6 +7,8 @@ const int kDefaultIntegerValue = 0;
 const bool kDefaultBooleanValue = false;
 const char* kDefaultStringValue = "Default";
 
+unsigned int SyncTrackFindKeys(const struct SyncTrack* inSyncTrack, unsigned int inPosition, struct SyncTrackKey outTrackKeys[2]);
+
 float InterpolateFloatKeys(const struct SyncTrackKey* inKeyA, const struct SyncTrackKey* inKeyB, unsigned int inPosition)
 {
     switch(inKeyA->InterpolationType)
@@ -54,29 +56,30 @@ float SyncTrackGetFloat(const struct SyncTrack* inSyncTrack, unsigned int inPosi
 {
     assert(inSyncTrack->Type == kTrackType_Float);
 
-    if(inSyncTrack->NumOfKeys == 0)
+    struct SyncTrackKey trackKeys[2];
+    unsigned int keyCount = SyncTrackFindKeys(inSyncTrack, inPosition, trackKeys);
+    switch(keyCount)
     {
-        // Return the default value since there's no keys
-        return kDefaultFloatValue;
+        case 0:
+        {
+            // No keys in track
+            return kDefaultFloatValue;
+        }
+        case 1:
+        {
+            // Single key value
+            return trackKeys[0].FloatData;
+        }
+        case 2:
+        {
+            // Interpolated key value
+            return InterpolateFloatKeys(&trackKeys[0], &trackKeys[1], inPosition);
+        }
     }
-    else if(inSyncTrack->NumOfKeys == 1)
-    {
-        // Don't even bother checking position here, just return the only value we have
-        struct SyncTrackKey* trackKey = &inSyncTrack->Keys[0];
-        assert(trackKey->ParentType == kTrackType_Float);
 
-        return trackKey->FloatData;
-    }
-    else
-    {
-        // Binary search for two closest keys here since we're sure we have atleast 2 keys
-        // @TODO
-
-        struct SyncTrackKey* keyA = 0;
-        struct SyncTrackKey* keyB = 0;
-        // Return the interpolated value of the two keys based on the position
-        return InterpolateFloatKeys(keyA, keyB, inPosition);
-    }
+    // This should never happen...
+    assert(false);
+    return kDefaultFloatValue;
 }
 
 int SyncTrackGetInteger(const struct SyncTrack* inSyncTrack, unsigned int inPosition)
@@ -98,4 +101,75 @@ const char* SyncTrackGetString(const struct SyncTrack* inSyncTrack, unsigned int
     assert(inSyncTrack->Type == kTrackType_String);
 
     return kDefaultStringValue;
+}
+
+unsigned int SyncTrackFindKeys(const struct SyncTrack* inSyncTrack, unsigned int inPosition, struct SyncTrackKey outTrackKeys[2])
+{
+    if(inSyncTrack->NumOfKeys == 0)
+    {
+        return 0;
+    }
+    else if(inSyncTrack->NumOfKeys == 1)
+    {
+        outTrackKeys[0] = inSyncTrack->Keys[0];
+        return 1;
+    }
+    else
+    {
+        // Binary search for two closest keys here since we're sure we have at least 2 keys
+
+        unsigned int minIndex = 0;
+        unsigned int maxIndex = inSyncTrack->NumOfKeys - 1;
+
+        while(minIndex < maxIndex)
+        {
+            unsigned int midIndex = (minIndex + maxIndex) / 2;
+            assert(midIndex < maxIndex);
+
+            if(inSyncTrack->Keys[midIndex].Position < inPosition)
+            {
+                minIndex = midIndex + 1;
+            }
+            else
+            {
+                maxIndex = midIndex;
+            }
+        }
+
+        // Assume we must have finished iteration if we've exited the loop
+        assert(minIndex == maxIndex);
+
+        unsigned int keyIndex = minIndex;
+        const struct SyncTrackKey* key = &inSyncTrack->Keys[keyIndex];
+
+        if(key->Position == inPosition)
+        {
+            // Found a direct match, simply return the key
+            outTrackKeys[0] = inSyncTrack->Keys[keyIndex];
+            return 1;
+        }
+        else
+        {
+            // Direct match not found, figure out what to do based on the surrounding keys
+            int direction = (key->Position - inPosition) > 0 ? 1 : -1; // We know this will never be zero since the case above wasn't taken
+            int secondKeyIndex = keyIndex + direction;
+            if(secondKeyIndex >= 0 && secondKeyIndex < inSyncTrack->NumOfKeys)
+            {
+                // We have a valid secondary key, return both
+
+                unsigned int minKeyIndex = keyIndex > secondKeyIndex ? secondKeyIndex : keyIndex;
+                unsigned int maxKeyIndex = keyIndex > secondKeyIndex ? keyIndex : secondKeyIndex;
+
+                outTrackKeys[0] = inSyncTrack->Keys[minKeyIndex];
+                outTrackKeys[1] = inSyncTrack->Keys[maxKeyIndex];
+                return 2;
+            }
+            else
+            {
+                // Our secondary key falls outside of the array, return the first key
+                outTrackKeys[0] = inSyncTrack->Keys[keyIndex];
+                return 1;
+            }
+        }
+    }
 }
